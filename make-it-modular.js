ac471a6d08cb30d317cce3a84fb6e2e715cc1640
KarenHippitaku <hippitaku@gmail.com>
2019-06-27 13:39:45 -0500

    Sixth exercise 2/2. (update) All options working. Solved.

--- make-it-modular.js
+++ make-it-modular.js
@@ -2,24 +2,28 @@ const myModule = require('./myModule.js');
 let ls = process.argv[2];
 let ext = process.argv[3]; //the dot it´s not given, so if you don´t add it the extension comparison won´t match :C
 
-//opc failled
-// console.log(myModule); //Whitout the following function, it trows an undefined line at the first position of the array... but why? .-.
-// myModule (ls, ext, (err, files) => { //now it works but just passes two of three tests. It asks for the callback call :P
-//   if (err) {
-//     console.error(err);
-//   }
-//   console.log(files);
-// });
+// console.log(myModule); //Whitout the following function, it trows an undefined line at the first position --
+//of the array... but why? .-.
+
+//opc 1
+myModule(ls, ext, (err, list) => { //now it works
+  if (err) {
+    console.error('Someone fuck it up', err);
+  }
+    list.forEach((file) => { //This line was missing, so it didn´t have a place to took the files from
+      console.log(file);
+  })
+});
 
 //opc2
-const callback = (err, list) => { //So I had to stablish de callback and call it
-  if (err) console.error(err);
-  list.forEach((file) => {
-    console.log(file);
-  })
-};
-
-myModule(ls, ext, callback);
+// const callback = (err, list) => { //This function can be called whatever you like, callback, filterFn, myModule, etc.
+//   if (err) console.error(err);
+//   list.forEach((file) => {
+//     console.log(file);
+//   })
+// };
+//
+// myModule(ls, ext, callback);
 
 //official solution
 // const filterFn = require('./myModule.js');
