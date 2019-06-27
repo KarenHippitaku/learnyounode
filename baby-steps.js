6aab1fde8b949f6eb8a5b1527d45ba6c80c06bde
KarenHippitaku <hippitaku@gmail.com>
2019-06-27 12:08:21 -0500

    Second exercise. Fully commented version, for testing

---
+++ baby-steps.js
@@ -0,0 +1,20 @@
+console.log(process.argv); //This didn´t work as expected and I do not know why :C
+// Number(process.argv[2])
+//
+// opc1
+// const argumentList = process.argv;
+// const addedArg = argumentList.slice(2,argumentList.length);
+//
+// let sum = 0;
+// let result = addedArg.forEach((value) => {
+//   sum += Number(value);
+//   return sum;
+// });
+// console.log(sum);
+//
+// official solution
+// let result = 0;
+// for (let i = 2; i <process.argv.length; i++) {
+//   result += Number(process.argv[i]);
+// };
+// console.log(result);
