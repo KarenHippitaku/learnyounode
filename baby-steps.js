cc72a1ce7f8a12e9e0bc1972c9ebe8c92a731490
KarenHippitaku <hippitaku@gmail.com>
2019-06-27 12:13:02 -0500

    Second exercise. Solved.

--- baby-steps.js
+++ baby-steps.js
@@ -1,17 +1,17 @@
-console.log(process.argv); //This didn´t work as expected and I do not know why :C
+// console.log(process.argv); //This didn´t work as expected and I do not know why :C
 // Number(process.argv[2])
-//
+
 // opc1
-// const argumentList = process.argv;
-// const addedArg = argumentList.slice(2,argumentList.length);
-//
-// let sum = 0;
-// let result = addedArg.forEach((value) => {
-//   sum += Number(value);
-//   return sum;
-// });
-// console.log(sum);
-//
+const argumentList = process.argv;
+const addedArg = argumentList.slice(2,argumentList.length);
+
+let sum = 0;
+let result = addedArg.forEach((value) => {
+  sum += Number(value);
+  return sum;
+});
+console.log(sum);
+
 // official solution
 // let result = 0;
 // for (let i = 2; i <process.argv.length; i++) {
